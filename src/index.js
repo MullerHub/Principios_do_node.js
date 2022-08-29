fbc32f31afc8ac1c1ad107b25f84da11539638e4
Murilo_Muller <murilomuller@protonmail.com>
2022-08-29 16:14:56 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
-
 const app = express()
 
 const customers = []
 
-app.use(express.json())
+// Middleware
+function verificaCPF(request, response, next) {
+  const { cpf } = request.headers
 
-/*  cpf - string
-    name - string
-    id - uuid  "yarn add uuid"
-    statement ["array"]       */
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' })
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
+app.use(express.json())
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
 
-  const customerExiste = customers.some(customer => customer.cpf === cpf)
+  const verificaCPF = customers.some(customer => customer.cpf === cpf)
 
-  if (customerExiste) {
+  if (verificaCPF) {
     return response.status(400).json({ error: 'Customer já existe!' })
   }
 
@@ -31,33 +40,23 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers
+// app.use(verificaCPF) se utilizar o app.use, tudo que vier abaixo passará por este middware, nesta aplicação irei utilizar o verificaCPF dentro de cada chamada necessaria
 
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' })
-  }
-
+app.get('/statement', verificaCPF, (request, response) => {
+  const {
+     customer } = request;
   return response.json(customer.statement)
 })
 
-/* 
 
-// Referencia do uso de Query Params
-app.put('/account/:id', (request, response) => {
-  const params = request.params
-  
+app.post('/deposit', verificaCPF, (request, response) => {
+  const { description, amount } = request.body;
 })
 
-app.patch('/account/:id', (request, response) => {
-
-})
-
+/*
 app.delete('/account/:id', (request, response) => {
 
-}) */
+}) 
 
 /*  Rota para rodar a aplicação, colocar no browser: localhost: "numero do app.listen abaixo"  /  "rota do site, que nessa aplicação seria /account"
 Ficaria no browser este link: http://localhost:3333/account
