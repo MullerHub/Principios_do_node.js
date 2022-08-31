f9488ffb9eabe643122a36e3187afe61fe95faef
Murilo_Muller <murilomuller@protonmail.com>
2022-08-31 10:22:40 -0300

    inserindo tipagem 3

--- Criando_API/src/routes/categories.routes.ts
+++ Criando_API/src/routes/categories.routes.ts
@@ -8,15 +8,7 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
   
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  })
-  categories.push(category)
-
   return response.status(201).send()
 })
 
