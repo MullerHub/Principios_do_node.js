a02dbc0bf88c812246f9bd76cf2328eb666cbada
Murilo_Muller <murilomuller@protonmail.com>
2022-08-31 17:20:45 -0300

    estudos sobre solid

---
+++ Criando_API/src/services/CreateCategoryService.ts
@@ -0,0 +1,25 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {
+  
+}
+
+  execute({ description, name }: IRequest): void {
+    const categoriesRepository = new CategoriesRepository();
+
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists) {
+      throw new Error("Categoria já existe")
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService }
