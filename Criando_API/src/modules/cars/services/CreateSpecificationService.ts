520bc613c87217b63faea86b338e1c5185dc6fbe
Murilo_Muller <murilomuller@protonmail.com>
2022-09-01 19:16:42 -0300

    Specification finished

--- Criando_API/src/modules/cars/services/CreateSpecificationService.ts
+++ Criando_API/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,29 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("to-do")
+  constructor(private specificationsRepository: ISpecificationRepository) {
+
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationsRepository = new SpecificationsRepository
+
+    const specificationAlreadyExists = this.specificationsRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!")
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    })
   }
 }
 
