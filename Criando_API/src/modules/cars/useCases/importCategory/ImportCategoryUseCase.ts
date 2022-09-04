e9c8a705b26c456d411a10040977f8d6799a25ed
Murilo_Muller <murilomuller@protonmail.com>
2022-09-04 15:47:30 -0300

    csv-parse adicionado e dando error

--- Criando_API/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Criando_API/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,20 @@
-
+import csvParse from 'csv-parse';
+import fs from 'fs';
 
 class ImportCategoryUseCase {
+  execute(file: Express.Multer.File): void {
 
+    const stream = fs.createReadStream(file.path);
+    
+/*  tá dando esse nesse csv-parse
 
-  execute(file: any) {
-    console.log(file)
+    const parseFile = csvParse();
+    
+    stream.pipe(parseFile)
+    
+    parseFile.on("data", async (line) => {
+      console.log(line)
+    })  */
   }
 }
 
--- Criando_API/src/modules/cars/useCases/importCategory/importCategoryController.ts
+++ Criando_API/src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -1,16 +1,16 @@
-import {Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
-  constructor(private importCategoryUseCase: ImportCategoryUseCase){}
+  constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    const {file} = request;
+    const { file } = request;
 
     this.importCategoryUseCase.execute(file);
-    console.log(file)
+
     return response.send()
   }
 }
 
-export { ImportCategoryController}
+export { ImportCategoryController }
